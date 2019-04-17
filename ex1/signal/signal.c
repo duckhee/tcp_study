46a8624c50aed41f190e27d55df4d7b96368d7c9
duckhee <fain930@yahoo.co>
2019-04-17 09:55:37 +0900

    tesitng

--- ex1/signal/signal.c
+++ ex1/signal/signal.c
@@ -11,7 +11,24 @@ void timeout(int sig)
     alarm(2);
 }
 
+void keycontrol(int sig)
+{
+    if(sig == SIGINT)
+    {
+        puts("CTRL+C pressed");
+    }
+}
+
 int main(int argc, char **argv)
 {
+    int i;
+    signal(SIGALRM, timeout);
+    signal(SIGINT, keycontrol);
+    alarm(2);
+    for(i = 0; i < 3; i++)
+    {
+        puts("wait...\r\n");
+        sleep(100);
+    }
     return 0;
 }
